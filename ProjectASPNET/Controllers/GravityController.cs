b2ad67346b08f65847c0dee7490f94ed4251ed06
minislel <marcin4kt@gmail.com>
2025-01-03 03:29:51 +0100

    Add project files.

---
+++ ProjectASPNET/Controllers/GravityController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectASPNET.Models;
+using X.PagedList;
+
+namespace ProjectASPNET.Controllers
+{
+	public class GravityController : Controller
+	{
+		private readonly GravityContext _context;		
+		public GravityController(GravityContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public async Task<IActionResult> Clients()
+		{
+			return View(await _context.Customers.ToListAsync());
+		}
+	}
+}
